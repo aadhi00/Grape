cc3e2daebdd0da2ae19c12c6cb5e315c6d322a47
Aadhithyan <aadhithyanaadhi@gmail.com>
2019-12-12 10:35:40 +0530

    success

---
+++ src/main/java/org/sample/Sample.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int f=10;
+		for(int i=0;i<f;i++) {
+			System.out.println(f);
+		}
+	}
+}
